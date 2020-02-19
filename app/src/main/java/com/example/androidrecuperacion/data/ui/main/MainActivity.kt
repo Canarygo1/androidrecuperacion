a159937c5ab70ad3fc57d4b69119dbded4dea2e0
Alejandro <Canarygo1@gmail.com>
2020-02-19 10:59:45 +0100

    first commit

---
+++ app/src/main/java/com/example/androidrecuperacion/data/ui/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.androidrecuperacion.data.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.androidrecuperacion.R
+
+class MainActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
